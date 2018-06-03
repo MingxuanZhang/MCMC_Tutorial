87cafaa495cb02326949a12562ae2a90e3702221
John Letey <john@Johns-MacBook.local>
2018-06-03 13:58:37 -0600

    doc: maybe???

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -110,7 +110,7 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ["../.."]
+#html_theme_path = ["../.."]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -135,7 +135,7 @@ html_title = "MCMC Tutorial Documentation"
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = '%b %d, %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.

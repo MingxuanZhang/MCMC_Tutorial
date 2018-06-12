009c112b0a17633b7bae96c4f2cdddd760e54cd1
John Letey <john.letey@colorado.edu>
2018-06-12 07:14:24 -0600

    updated the repo structure

--- scripts/build.sh
+++
@@ -1,6 +0,0 @@
-# Copy figures over so that notebook page will render them
-rm -rf docs/source/figs
-mkdir docs/source/figs
-cp -r figs/ docs/source/figs
-# Build sphinx!
-sphinx-build docs/source docs/build/html
